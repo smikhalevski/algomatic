9b9d37fc8ddaf794fd8d3798ab5a4c39cf23025c
Savva Mikhalevski <smikhalevski@gmail.com>
2022-02-02 23:00:35 +0300

    Inline spline component shifts

--- src/main/spline.ts
+++ src/main/spline.ts
@@ -1,9 +1,11 @@
-const A = 0;
-const B = 1;
-const C = 2;
-const D = 3;
-const X = 4;
-const L = 5;
+const enum Shift {
+  A = 0,
+  B = 1,
+  C = 2,
+  D = 3,
+  X = 4,
+  L = 5,
+}
 
 const tempAs: number[] = [];
 const tempBs: number[] = [];
@@ -11,24 +13,29 @@ const tempSplines: number[] = [];
 
 /**
  * Computes splines for `xs` and `ys`.
+ *
+ * @param xs X coordinates of pivot points.
+ * @param ys Y coordinates of pivot points.
+ * @param splines Mutable array that would be populated with spline components. If omitted, a temp array is used.
+ * @returns The splines array.
  */
-export function populateSplines(xs: number[], ys: number[], splines = tempSplines): number[] {
+export function populateSplines(xs: readonly number[], ys: readonly number[], splines = tempSplines): number[] {
   const n = xs.length;
   const as = tempAs;
   const bs = tempBs;
 
-  for (let i = 0; i < n * L; ++i) {
+  for (let i = 0; i < n * Shift.L; ++i) {
     splines[i] = 0;
   }
   for (let i = 0; i < n - 1; ++i) {
     as[i] = bs[i] = 0;
   }
   for (let i = 0; i < n; ++i) {
-    splines[i * L + X] = xs[i];
-    splines[i * L + A] = ys[i];
+    splines[i * Shift.L + Shift.X] = xs[i];
+    splines[i * Shift.L + Shift.A] = ys[i];
   }
 
-  splines[C] = splines[(n - 1) * L + C] = 0;
+  splines[Shift.C] = splines[(n - 1) * Shift.L + Shift.C] = 0;
 
   for (let i = 1; i < n - 1; ++i) {
     const xsi = xs[i];
@@ -45,16 +52,16 @@ export function populateSplines(xs: number[], ys: number[], splines = tempSpline
   }
 
   for (let i = n - 2; i > 0; --i) {
-    splines[i * L + C] = as[i] * splines[(i + 1) * L + C] + bs[i];
+    splines[i * Shift.L + Shift.C] = as[i] * splines[(i + 1) * Shift.L + Shift.C] + bs[i];
   }
 
   for (let i = n - 1; i > 0; --i) {
     const dx = xs[i] - xs[i - 1];
-    const sc1 = splines[(i - 1) * L + C];
-    const sc2 = splines[i * L + C];
+    const sc1 = splines[(i - 1) * Shift.L + Shift.C];
+    const sc2 = splines[i * Shift.L + Shift.C];
 
-    splines[i * L + D] = (sc2 - sc1) / dx;
-    splines[i * L + B] = dx * (2 * sc2 + sc1) / 6 + (ys[i] - ys[i - 1]) / dx;
+    splines[i * Shift.L + Shift.D] = (sc2 - sc1) / dx;
+    splines[i * Shift.L + Shift.B] = dx * (2 * sc2 + sc1) / 6 + (ys[i] - ys[i - 1]) / dx;
   }
 
   return splines;
@@ -63,38 +70,38 @@ export function populateSplines(xs: number[], ys: number[], splines = tempSpline
 /**
  * Computes cubic spline for `xs` and `ys` at `x` and returns interpolated `y`.
  *
- * @param xs X coordinates of points.
- * @param ys Y coordinates of points.
+ * @param xs X coordinates of pivot points.
+ * @param ys Y coordinates of pivot points.
  * @param x X coordinate of interpolated points.
  * @param splines Optional pre-computed splines. If omitted then splines are computed from `xs` and `ys` on the fly.
  * @returns Interpolated Y coordinate.
  *
  * @see {@link https://en.wikipedia.org/wiki/Spline_(mathematics)#Algorithm_for_computing_natural_cubic_splines Algorithm for computing natural cubic splines}
  */
-export function spline(xs: number[], ys: number[], x: number, splines = populateSplines(xs, ys)): number {
+export function spline(xs: readonly number[], ys: readonly number[], x: number, splines: readonly number[] = populateSplines(xs, ys)): number {
   const n = xs.length;
 
   let s: number;
 
-  if (x <= splines[X]) {
+  if (x <= splines[Shift.X]) {
     s = 0;
-  } else if (x >= splines[(n - 1) * L + X]) {
-    s = (n - 1) * L;
+  } else if (x >= splines[(n - 1) * Shift.L + Shift.X]) {
+    s = (n - 1) * Shift.L;
   } else {
     let i = 0;
     let j = n - 1;
 
     while (i + 1 < j) {
       const k = i + (j - i) / 2;
-      if (x <= splines[k * L + X]) {
+      if (x <= splines[k * Shift.L + Shift.X]) {
         j = k;
       } else {
         i = k;
       }
     }
-    s = j * L;
+    s = j * Shift.L;
   }
 
-  const dx = x - splines[s + X];
-  return splines[s + A] + (splines[s + B] + (splines[s + C] / 2 + splines[s + D] * dx / 6) * dx) * dx;
+  const dx = x - splines[s + Shift.X];
+  return splines[s + Shift.A] + (splines[s + Shift.B] + (splines[s + Shift.C] / 2 + splines[s + Shift.D] * dx / 6) * dx) * dx;
 }
