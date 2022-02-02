fb3e372a72c409629953f7712ea51cc3b1467d19
Savva Mikhalevski <smikhalevski@gmail.com>
2022-02-02 21:43:45 +0300

    Less objects to calc spline

--- src/main/createSplineMap.ts
+++ src/main/createSplineMap.ts
@@ -6,28 +6,40 @@ interface ISpline {
   x: number;
 }
 
-function calcSplines(xs: Array<number>, ys: Array<number>): Array<ISpline> {
-  const splines = new Array<ISpline>();
+const A = 0;
+const B = 1;
+const C = 2;
+const D = 3;
+const X = 4;
+const L = 5;
 
-  for (let i = 0; i < xs.length; i++) {
-    splines[i] = {a: 0, b: 0, c: 0, d: 0, x: 0};
-  }
+const as: number[] = [];
+const bs: number[] = [];
 
+function calcSplines(xs: number[], ys: number[], splines: number[]): number[] {
   const n = xs.length;
-  for (let i = 0; i < n; i++) {
-    splines[i].x = xs[i];
-    splines[i].a = ys[i];
-  }
-  splines[0].c = splines[n - 1].c = 0;
 
-  const as = new Array(n - 1).fill(0);
-  const bs = new Array(n - 1).fill(0);
+  for (let i = 0; i < n * L; ++i) {
+    splines[i] = 0;
+  }
+  for (let i = 0; i < n - 1; ++i) {
+    as[i] = bs[i] = 0;
+  }
+
+  for (let i = 0; i < n; ++i) {
+    splines[i * L + X] = xs[i];
+    splines[i * L + A] = ys[i];
+  }
+  splines[C] = splines[(n - 1) * L + C] = 0;
 
   for (let i = 1; i < n - 1; ++i) {
-    const a = xs[i] - xs[i - 1];
-    const b = xs[i + 1] - xs[i];
+    const xsi = xs[i];
+    const ysi = ys[i];
+
+    const a = xsi - xs[i - 1];
+    const b = xs[i + 1] - xsi;
     const c = 2 * (a + b);
-    const f = 6 * ((ys[i + 1] - ys[i]) / b - (ys[i] - ys[i - 1]) / a);
+    const f = 6 * ((ys[i + 1] - ysi) / b - (ysi - ys[i - 1]) / a);
     const z = (a * as[i - 1] + c);
 
     as[i] = -b / z;
@@ -35,13 +47,13 @@ function calcSplines(xs: Array<number>, ys: Array<number>): Array<ISpline> {
   }
 
   for (let i = n - 2; i > 0; --i) {
-    splines[i].c = as[i] * splines[i + 1].c + bs[i];
+    splines[i * L + C] = as[i] * splines[(i + 1) * L + C] + bs[i];
   }
 
   for (let i = n - 1; i > 0; --i) {
     const dx = xs[i] - xs[i - 1];
-    splines[i].d = (splines[i].c - splines[i - 1].c) / dx;
-    splines[i].b = dx * (2 * splines[i].c + splines[i - 1].c) / 6 + (ys[i] - ys[i - 1]) / dx;
+    splines[i * L + D] = (splines[i * L + C] - splines[(i - 1) * L + C]) / dx;
+    splines[i * L + B] = dx * (2 * splines[i * L + C] + splines[(i - 1) * L + C]) / 6 + (ys[i] - ys[i - 1]) / dx;
   }
 
   return splines;
@@ -52,33 +64,35 @@ function calcSplines(xs: Array<number>, ys: Array<number>): Array<ISpline> {
  *
  * @see https://en.wikipedia.org/wiki/Spline_(mathematics)#Algorithm_for_computing_natural_cubic_splines
  */
-export function createSplineMap(xs: Array<number>, ys: Array<number>): (x: number) => number {
-  const splines = calcSplines(xs, ys);
-  const n = splines.length;
+export function createSplineMap(xs: number[], ys: number[]): (x: number) => number {
+  const n = xs.length;
+  const splines: number[] = [];
+
+  calcSplines(xs, ys, splines);
 
   return (x) => {
-    let s;
+    let s: number;
 
-    if (x <= splines[0].x) {
-      s = splines[0];
-    } else if (x >= splines[n - 1].x) {
-      s = splines[n - 1];
+    if (x <= splines[X]) {
+      s = 0;
+    } else if (x >= splines[(n - 1) * L + X]) {
+      s = (n - 1) * L;
     } else {
       let i = 0;
       let j = n - 1;
 
       while (i + 1 < j) {
         const k = i + (j - i) / 2;
-        if (x <= splines[k].x) {
+        if (x <= splines[k * L + X]) {
           j = k;
         } else {
           i = k;
         }
       }
-      s = splines[j];
+      s = j * L;
     }
 
-    const dx = x - s.x;
-    return s.a + (s.b + (s.c / 2 + s.d * dx / 6) * dx) * dx;
+    const dx = x - splines[s + X];
+    return splines[s + A] + (splines[s + B] + (splines[s + C] / 2 + splines[s + D] * dx / 6) * dx) * dx;
   };
 }
